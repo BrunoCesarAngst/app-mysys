d8f2d1f2094f8acf271b1787f42e9668876daa7c
BrunoCesarAngst <bruno.angst@rede.ulbra.br>
2023-04-27 03:36:57 -0300

    tomando jeito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,200 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+//  criar uma página com o nome Home
+//  dividida em duas colunas
+//  na primeira coluna com 1/3 do tamanho, ter um input para adicionar tarefas com o foco inicial com o enter,
+//  adicionar a tarefa na lista
+//  abaixo desse input, vai tem uma lista de links para outras páginas do site com o nome bem a esquerda e a quantidade
+// de tarefas que tem nessa página bem a direita
+//  na segunda coluna com 2/3 do tamanho, ter uma lista de tarefas editável que mostra o texto da tarefa, a data de
+// criação e um botão para remover a tarefa, um botão para marcar a tarefa como completa ou incompleta e um botão para
+// editar o texto da tarefa
 
-const inter = Inter({ subsets: ['latin'] })
+'use client';
+
+import React, { useState } from 'react';
+
+// criar um tipo Task com os campos id, text, completed, data de criação e data de atualização
+type Task = {
+  id: number;
+  text: string;
+  createdAt: string;
+  updatedAt: string;
+  discerned: boolean;
+  fastAction?: boolean;
+  incubate?: boolean;
+  reference?: boolean;
+  trash?: boolean;
+  delegated?: boolean;
+  actionDate?: boolean;
+  context?: boolean;
+  project?: boolean;
+  completed?: boolean;
+};
 
 export default function Home() {
+  // criar um estado para armazenar as tarefas da lista que respeitem o tipo Task
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  // criar um estado para armazenar o valor do input
+  const [inputValue, setInputValue] = useState('');
+
+  // criar um estado para armazenar o índice da tarefa que está sendo editada
+  const [editIndex, setEditIndex] = useState<number | null>(null);
+
+  // criar um estado para armazenar o valor do input de edição
+  const [editValue, setEditValue] = useState<string>('');
+
+  // criar um estado para motrar ou não um modal
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  // criar uma referência para o input de adicionar tarefas
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  // criar uma função para lidar com o evento de pressionar uma tecla no input
+  const handleInputKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === 'Enter') {
+      setTasks(prevTasks => [
+        ...prevTasks,
+        {
+          id: Math.floor(Math.random() * 1000),
+          text: inputValue,
+          createdAt: new Date().toLocaleTimeString(),
+          updatedAt: new Date().toLocaleTimeString(),
+          discerned: false,
+          completed: false,
+        },
+      ]);
+      setInputValue('');
+      event.currentTarget.value = '';
+      inputRef.current?.focus();
+    }
+  };
+
+  // criar uma função para lidar com o evento de pressionar uma tecla no input de edição
+  const handleItemClicked = (index: number) => {
+    setEditIndex(index);
+    setEditValue(tasks[index].text);
+  };
+
+  // criar uma função que antes de atualizar o valor do input de edição abre um alarta para confirmar a ação
+  const handleEditSaveClick = () => {
+    if (editIndex !== null) {
+      if (window.confirm('Deseja salvar as alterações?')) {
+        setTasks(prevTasks => {
+          const newTasks = [...prevTasks];
+          newTasks[editIndex] = {
+            ...newTasks[editIndex],
+            text: editValue,
+            updatedAt: new Date().toLocaleTimeString(),
+          };
+          return newTasks;
+        }),
+          setEditIndex(null),
+          setEditValue('');
+      }
+    }
+  };
+
+  // criar uma função para remover uma tarefa da lista
+  const removeTask = (id: number) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  // criar uma função para marcar uma tarefa como completa
+  const completeTask = (id: number) => {
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          return { ...task, completed: true, updatedAt: new Date().toLocaleTimeString() };
+        }
+        return task;
+      })
+    );
+  };
+
+  // criar uma função para marcar uma tarefa como incompleta
+  const incompleteTask = (id: number) => {
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          return { ...task, completed: false, updatedAt: new Date().toLocaleTimeString() };
+        }
+        return task;
+      })
+    );
+  };
+
+  // retornar o componente com o input de adicionar tarefas e a lista de tarefas com os botões de remover, editar, marcar como completa ou incompleta
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen">
+      <div className="p-3 w-1/3 bg-gray-100">
+        <h1 className="text-2xl font-bold">Adicionar Coisas</h1>
+        <input
+          className="w-full h-16 px-6"
+          type="text"
+          onChange={e => setInputValue(e.target.value)}
+          placeholder="Pressione enter para adicionar alguma coisa"
+          onKeyDown={handleInputKeyPress}
+        />
+        <div className="p-1">
+          <h1 className="text-2xl font-bold">Listas</h1>
+          <ul className="p-1">
+            <li className="flex justify-between">
+              <a href="/" className="">
+                Inbox
+              </a>
+              <span className="">{tasks.length}</span>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {showModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+          <div className="bg-white p-3 rounded">
+            <h1 className="text-2xl font-bold">Modal</h1>
+            <p>Conteúdo do modal</p>
+            <button onClick={() => setShowModal(false)}>Fechar</button>
+          </div>
+        </div>
+      )}
+      <button onClick={() => setShowModal(true)}>Abrir</button>
+      <div className="p-3 w-2/3 bg-gray-200">
+        <h1 className="text-2xl font-bold">Inbox</h1>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={task.id}>
+              {editIndex === index ? (
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={e => setEditValue(e.target.value)}
+                />
+              ) : (
+                <span
+                  className={`cursor-pointer ${
+                    task.completed ? 'line-through' : ''
+                  }`}
+                  onClick={() => handleItemClicked(index)}
+                >
+                  {task.text} - {task.createdAt} - {task.updatedAt}
+                </span>
+              )}
+              <button onClick={() => removeTask(task.id)}>🗑️</button>
+              {editIndex === index ? (
+                <button onClick={handleEditSaveClick}>💾</button>
+              ) : (
+                <button onClick={() => handleItemClicked(index)}>✏️</button>
+              )}
+              {task.completed ? (
+                <button onClick={() => incompleteTask(task.id)}>✅</button>
+              ) : (
+                <button onClick={() => completeTask(task.id)}>◻︎</button>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
